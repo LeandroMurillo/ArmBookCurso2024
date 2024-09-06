3a5ddcbb86d9c2edf61285ed4df586d0bbde28d8
LeandroMurillo <leandromurillo00@gmail.com>
2024-09-06 19:53:17 +0000

    se ha implementado la entrada de opciones al menu principal (revisar)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 // =====[Declaration and initialization of public global variables]===========
 
-#define TIME_MS 100
+#define TIME_MS 100 //lo vamos a tener que hacer bloqueante a proposito...
 #define VISIT_TIME 5000 // habría que ver cómo podemos hacer para mandarle 2 minutos. Sin mucho más drama. Creería que se puede
 #define BLINKING_TIME_TAKING_PICTURE 500 //si le ponemos medio segundo podriamos más o menos determinar cuantas veces en un segundo tiene que parpadear
 #define BLINKING_TIME_PLAYING_AUDIO 500
@@ -19,10 +19,6 @@ DigitalOut recordingAudioLed(LED3);
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
 // =====[Declaration and initialization of public global variables]===========
-
-bool ringBellState = OFF; //indica que el timbre ha sido presionado,
-// la idea es que cuando se presione el pulsador en la placa, esta variable se active
-// si esta variable está activada, ahi recien vamos a encender el LED del timbre, asi sería
 
 bool doorBellButtonState = OFF;
 bool optionState = OFF;
@@ -95,6 +91,7 @@ void doorbellActivationUpdate()
         
         if(optionState){
             chooseOption();
+            //optionState = OFF;
         }
         
 
@@ -103,6 +100,80 @@ void doorbellActivationUpdate()
             ellapsed_time = 0;
             doorBellButtonState = OFF;
             optionState = ON;
+        }
+    }
+}
+
+//okey ahora toca implementar los comandos de la UART y con eso ya estaríamos 
+
+void chooseOption() 
+{
+    
+    optionsMenu();
+
+    char receivedChar = '\0';
+    if( uartUsb.readable() ) {
+        uartUsb.read( &receivedChar, 1 );
+
+        switch (receivedChar) {
+            case '1':  // Opción 1
+                uartUsb.write("Elegiste la opcion 1\r\n\r\n", 25);
+                break;
+
+            case '2':  // Opción 2
+                uartUsb.write("Elegiste la opcion 2\r\n\r\n", 25);
+                break;
+
+            default:  // Opción inválida
+                uartUsb.write("Por favor, elija una opción válida.\r\n", 37);
+                break;
+        }
+    }
+    
+}
+
+void optionsMenu()
+{
+    uartUsb.write("Elija una de las opciones disponibles:\r\n\r\n", 40);
+    uartUsb.write("Presione '1' para enviar una respuesta pre-grabada\r\n\r\n", 53);
+    uartUsb.write("Presione '2' para enviar un mensaje de voz\r\n\r\n", 47);
+
+}
+
+void option1Menu()
+{
+    uartUsb.write( "Usted ha elegido la opción 1. Elija una de las respuestas disponibles:\r\n", 21 );
+    uartUsb.write( "'1' - Buen día, si ¿qué necesita?\r\n\r\n", 36 );
+    uartUsb.write( "'2' - otra respuesta generica...'\r\n\r\n", 36 );
+    uartUsb.write( "'0' - VOLVER AL MENU PRINCIPAL\r\n\r\n", 36 );
+}
+
+
+
+/*void option1()
+{
+    option1Menu();
+
+    char receivedChar = '\0';  // Variable para almacenar el carácter recibido
+    int numMensajes = sizeof(mensajes) / sizeof(mensajes[0]);  // Número total de mensajes
+
+    if (uartUsb.readable()) {
+        uartUsb.read(&receivedChar, 1);  // Leer un carácter de UART
+
+        int index = receivedChar - '1';  // Convertir el carácter a índice ('1' -> 0, '2' -> 1, etc.)
+
+        if (index >= 0 && index < numMensajes) {
+            uartUsb.write(mensajes[index], mensajesLengths[index]);  // Usar el array de longitudes
+            playingAudioLed = ON;  // Encender el LED de reproducción
+            delay(BLINKING_TIME_PLAYING_AUDIO);  // Parpadear LED
+            playingAudioLed = OFF;  // Apagar el LED
+        } 
+        else if (receivedChar == '0') {
+            chooseOption();  // Volver al menú principal
+        } 
+        else {
+            uartUsb.write("Por favor, elija una opción válida.\r\n", 35);
+            option1();  // Volver a llamar a option1
         }
     }
 }
@@ -136,47 +207,6 @@ void chooseOption()
     }
 }
 
-
-void optionsMenu()
-{
-    uartUsb.write("Elija una de las opciones disponibles:\r\n\r\n", 40);
-    uartUsb.write("Presione '1' para enviar una respuesta pre-grabada\r\n\r\n", 53);
-    uartUsb.write("Presione '2' para enviar un mensaje de voz\r\n\r\n", 47);
-
-}
-
-
-
-/*void option1()
-{
-    option1Menu();
-
-    char receivedChar = '\0';  // Variable para almacenar el carácter recibido
-    int numMensajes = sizeof(mensajes) / sizeof(mensajes[0]);  // Número total de mensajes
-
-    if (uartUsb.readable()) {
-        uartUsb.read(&receivedChar, 1);  // Leer un carácter de UART
-
-        int index = receivedChar - '1';  // Convertir el carácter a índice ('1' -> 0, '2' -> 1, etc.)
-
-        if (index >= 0 && index < numMensajes) {
-            uartUsb.write(mensajes[index], mensajesLengths[index]);  // Usar el array de longitudes
-            playingAudioLed = ON;  // Encender el LED de reproducción
-            delay(BLINKING_TIME_PLAYING_AUDIO);  // Parpadear LED
-            playingAudioLed = OFF;  // Apagar el LED
-        } 
-        else if (receivedChar == '0') {
-            chooseOption();  // Volver al menú principal
-        } 
-        else {
-            uartUsb.write("Por favor, elija una opción válida.\r\n", 35);
-            option1();  // Volver a llamar a option1
-        }
-    }
-}
-
-
-
 void option1Menu2()
 {
     uartUsb.write("Usted ha elegido la opción 1. Elija una de las respuestas disponibles:\r\n", 21);
@@ -193,13 +223,7 @@ void option1Menu2()
 }
 
 
-void option1Menu()
-{
-    uartUsb.write( "Usted ha elegido la opción 1. Elija una de las respuestas disponibles:\r\n", 21 );
-    uartUsb.write( "'1' - Buen día, si ¿qué necesita?\r\n\r\n", 36 );
-    uartUsb.write( "'2' - otra respuesta generica...'\r\n\r\n", 36 );
-    uartUsb.write( "'0' - VOLVER AL MENU PRINCIPAL\r\n\r\n", 36 );
-}
+
 
 void option2()
 {
